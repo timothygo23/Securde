5293792ff713605daf44812ebc9c1169b0d4b3bc
timothygo23 <timothy_go@dlsu.edu.ph>
2018-03-01 22:06:29 +0800

    shows available sizes now

--- src/main/webapp/resources/js/servicejs/product.js
+++ src/main/webapp/resources/js/servicejs/product.js
@@ -1,3 +1,5 @@
+var productAvailability;
+
 function loadBanner(catalog, product){
 	let catalogPath = "catalog?catalog=" + catalog.catalog_id;
 	
@@ -7,22 +9,58 @@ function loadBanner(catalog, product){
 	$("#directory").append(product.product_name);
 }
 
-function loadProduct(product, productAvailability){
+function loadProduct(product){
 	var price = "P" + product.price + ".00";
 	
 	$("#productName").html(product.product_name);
 	$("#productBrandName").html(product.brand_name);
 	$("#productPrice").html(price);
 	$("#productDescription").html(product.product_description);
+	
+}
+
+function getQuantity(size){
+	for(var i = 0; i < productAvailability.length; i++){
+		let pAvail = productAvailability[i];
+		if(pAvail.size == size)
+			return pAvail.quantity;
+	}
+	// returns 0 if there's no available size
+	return 0;
+}
+
+function loadProductAvailability(){
+	var size = $('#sizeSelect').find(':selected').val();
+	var quantity = getQuantity(size);
+	
+	if(quantity <= 0){
+		//out of stock
+		//hide quantityCartContainer and availability
+		$("#quantityCartContainer").hide();
+		$("#availability").hide();
+		//show out of stock div
+		$("#outOfStock").show();
+	}else{
+		//in stock
+		$("#available").html(quantity+"");
+
+		//hide out of stock div
+		$("#outOfStock").hide();
+		//show quantityCartContainer and availability
+		$("#quantityCartContainer").show();
+		$("#availability").show();
+	}
 }
 
 function loadProductInfo(data){
 	var catalog = data.catalog;
 	var product = data.product;
-	var productAvailability = data.productAvailability;
+	
+	productAvailability = data.productAvailability;
 	
 	loadBanner(catalog, product);
-	loadProduct(product, productAvailability);
+	loadProduct(product);
+	loadProductAvailability();
 }
 
 function getProductInfo(){
